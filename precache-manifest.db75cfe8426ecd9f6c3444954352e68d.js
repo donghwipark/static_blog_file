6efc4dd7447cf38911b6f6decf3164350c3f9310
Donghwi Park <donghwipark@gmail.com>
2020-02-02 12:50:15 +0900

    add build

---
+++ precache-manifest.db75cfe8426ecd9f6c3444954352e68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0669164a4765cbc8d4886bb5997743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "146584870c4e63f87863",
+    "url": "/static/css/2.b03023ee.chunk.css"
+  },
+  {
+    "revision": "f093101623e49765825a",
+    "url": "/static/css/main.e03ed78b.chunk.css"
+  },
+  {
+    "revision": "146584870c4e63f87863",
+    "url": "/static/js/2.c84d853a.chunk.js"
+  },
+  {
+    "revision": "f093101623e49765825a",
+    "url": "/static/js/main.6e3d07b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
